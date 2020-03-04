4c33c7d593bf660ee648b259a3bbc71cf2ec6e24
jonathanw82 <jonwhewaycode@outlook.com>
2020-03-04 17:30:27 +0000

    added some functinality to album search

--- assets/js/search_artist.js
+++ assets/js/search_artist.js
@@ -1,45 +1,46 @@
+/*
+function displaydatainmypage(artist_search) {
 
-function displaydatainmypage(music_search) {
-
-     console.log(music_search);
-
-    for (var i = 0; i < music_search.data.length; i++) {
-        artist_name = music_search.data[0].artist.name;
-        song_name = music_search.data[0].title;
-        album_name = music_search.data[0].album.title;
-        picture = music_search.data[0].artist.picture_medium;
-        preview = music_search.data[0].preview;
+     console.log(artist_search);
+/*
+    for (var i = 0; i < artist_search.data.length; i++) {
+        artist_name = music_search.data[i].artist.name;
+        song_name = music_search.data[i].title;
+        album_name = music_search.data[i].album.title;
+        picture = music_search.data[i].artist.picture_medium;
+        preview = music_search.data[i].preview;
 
         var artist_name = document.getElementById("artist_name").innerHTML = artist_name;
         var song_name = document.getElementById("song_name").innerHTML = song_name;
         var album_name = document.getElementById("album_name").innerHTML = album_name;
         var picture = document.getElementById("picture").src = picture;
-        var preview = document.getElementById("preview").src = preview;
-       
+       var preview = document.getElementById("preview").src = preview;
     };
 
 };
 
-function search() {
-    var user_input = document.getElementById("user_input").value;
+
+
+
+function search_artist() {
+    var user_input_artist = document.getElementById("user_input_artist").value;
     var api_request = new XMLHttpRequest();
 
     api_request.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var the_response = api_request.responseText;
-            var music_search = JSON.parse(the_response);                 
-            displaydatainmypage(music_search);
+            var artist_search = JSON.parse(the_response);                 
+            displaydatainmypage(artist_search);
         }
         else {
             console.log("this stuff is not working");
         }
     };
-    api_request.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + user_input);
+    api_request.open("GET", "https://deezerdevs-deezer.p.rapidapi.com/artist/" + user_input_artist);
     api_request.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
     api_request.setRequestHeader("x-rapidapi-key", "ef9686a9b9msh4dbce73327763a8p14d988jsn38434b35145a");
     api_request.send();
 }
+console.log(user_input_artist)
 
-
-
-
+*/
