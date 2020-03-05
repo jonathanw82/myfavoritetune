fa194edd231044b67dd038b04a1e4ad7ec9764ca
jonathanw82 <jonwhewaycode@outlook.com>
2020-03-05 17:28:25 +0000

    working Album art caroucel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,6 @@
 
 function displaydatainmypage(music_search) {
-
-console.log(music_search)
+    
     for (var i = 0; i < music_search.data.length; i++) {
         artist_name = music_search.data[i].artist.name;
         //song_name = music_search.data[i].title;
@@ -9,14 +8,14 @@ console.log(music_search)
         picture = music_search.data[i].artist.picture_medium;
         preview = music_search.data[i].preview;
 
+           // console.log(music_search)
+
         var artist_name = document.getElementById("artist_name").innerHTML += artist_name + "<br>";
         //var song_name = document.getElementById("song_name").innerHTML = song_name + "<br>";
         //var album_name = document.getElementById("album_name").innerHTML += album_name + "<br>";
         var picture = document.getElementById("picture").src = picture;
         var preview = document.getElementById("preview").src = preview;
-
     };
-
 };
 
 /* this.contact_number.value = Math.random() * 100000 | 0;
@@ -49,6 +48,24 @@ function search() {
     api_request.setRequestHeader("x-rapidapi-host", "deezerdevs-deezer.p.rapidapi.com");
     api_request.setRequestHeader("x-rapidapi-key", "ef9686a9b9msh4dbce73327763a8p14d988jsn38434b35145a");
     api_request.send();
+
+    // Constructing the suggestion engine
+    var user_input = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: music_search
+    });
+    // Initializing the typeahead
+    $('.typeahead').typeahead({
+        hint: true,
+        highlight: true, /* Enable substring highlighting */
+        minLength: 1 /* Specify minimum characters required for showing result */
+    },
+        {
+            name: 'user_input',
+            source: music_search
+        });
+
 }
 
 
---
+++ assets/js/typeahead.js
@@ -0,0 +1,21 @@
+/*
+  // Constructing the suggestion engine
+   var  user_input = new Bloodhound({
+        datumTokenizer: Bloodhound.tokenizers.whitespace,
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        local: user_input
+    });
+
+
+
+// Initializing the typeahead
+    $('.typeahead').typeahead({
+        hint: true,
+        highlight: true, /* Enable substring highlighting */
+     //   minLength: 1 /* Specify minimum characters required for showing result */
+  //  },
+  //   {
+   //     name: 'user_input',
+   //     source: music_search
+   // });
+
