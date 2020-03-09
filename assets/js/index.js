09e6fa73030955c2c906f1983eb9d562f7048d2e
jonathanw82 <jonwhewaycode@outlook.com>
2020-03-09 13:08:52 +0000

    rewrite index.js to remove bad code and remove caroucel from html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,12 @@
-
 function displaydatainmypage(music_search) {
 
     let albums = [];
     let albums_name = [];
 
     for (var i = 0; i < music_search.data.length; i++) {
-        artist_name = music_search.data[i].artist.name;
-        album_name_indentity = music_search.data[i].album.title;
-        artistid = music_search.data[i].artist.id;
-        picture = music_search.data[i].artist.picture_medium;
-        preview = music_search.data[i].preview;
 
         for (i = 0; i < music_search.data.length; i++) {
+
             var album = music_search.data[i].album;
             var album_name = music_search.data[i].album.title;
 
@@ -21,47 +16,19 @@ function displaydatainmypage(music_search) {
                 albums.push(album);
             }
         }
-       // console.log(music_search)
-
-        document.getElementById("artist_name").innerHTML = artist_name + "<br>";
-        document.getElementById("album_name").innerHTML = album_name_indentity + "<br>";
-        document.getElementById("artistid").innerHTML = artistid;
-        document.getElementById("picture").src = picture;
-        document.getElementById("preview").src = preview;
-
-
-        for (let i = 0; i < music_search.data.length; i++) {
-
-            albumart1 = music_search.data[i].album.cover_medium;
-            albumart2 = music_search.data[i].album.cover_medium;
-            albumart3 = music_search.data[i].album.cover_medium;
-            albumart4 = music_search.data[i].album.cover_medium;
-            albumart5 = music_search.data[i].album.cover_medium;
-            albumart6 = music_search.data[i].album.cover_medium;
-            albumart7 = music_search.data[i].album.cover_medium;
-            albumart8 = music_search.data[i].album.cover_medium;
-            albumart9 = music_search.data[i].album.cover_medium;
-            albumart10 = music_search.data[i].album.cover_medium;
-
-            var albumart1Div = document.getElementById("albumart1").src = albumart1;           // caroucel artwork 
-            var albumart2Div = document.getElementById("albumart2").src = albumart2;
-            var albumart3Div = document.getElementById("albumart3").src = albumart3;
-            var albumart4Div = document.getElementById("albumart4").src = albumart4;
-            var albumart5Div = document.getElementById("albumart5").src = albumart5;
-            var albumart6Div = document.getElementById("albumart6").src = albumart6;
-            var albumart7Div = document.getElementById("albumart7").src = albumart7;
-            var albumart8Div = document.getElementById("albumart8").src = albumart8;
-            var albumart9Div = document.getElementById("albumart9").src = albumart9;
-            var albumart10Div = document.getElementById("albumart10").src = albumart10;
-        }
-        console.log(albums_name)
     }
-    var album_list = document.getElementById("album_list");                                     // get the album title and id and display just wrong ******data
+
+    console.log("Array 'albums_names:'");
+    console.log(albums_name);
+    console.log("Array 'albums:'");    
+    console.log(albums);
+
+        var album_list = document.getElementById("album_list");                                     // get the album title and id and display just wrong ******data
+    
     for (i = 0; i < albums.length; i++) {
-        this_album_string = `<div>  ${album.title} ${album.id} </div>`;
+        this_album_string = `<div><p>${albums[i].id}</p><br><p>${albums[i].title}</p><img src="${albums[i].cover}"></div>`;
         album_list.innerHTML += this_album_string;
     }
-   // console.log(this_album_string);
 }
 
 function search() {
@@ -84,5 +51,3 @@ function search() {
     api_request.send();
 
 }
-
-
